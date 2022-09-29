a2151d220cc875b7a1aae6a9684933770b93d5d0
hyeongcheolkim <tori1753@naver.com>
2022-09-30 07:18:28 +0900

    Session 수정

--- src/main/java/NaNSsoGong/MrDaeBakDining/domain/session/SessionConst.java
+++ src/main/java/NaNSsoGong/MrDaeBakDining/domain/session/SessionConst.java
@@ -4,5 +4,4 @@ public interface SessionConst {
     static final String LOGIN_CLIENT = "loginClient";
     static final String LOGIN_CHEF = "loginChef";
     static final String LOGIN_RIDER = "loginRider";
-    static final String LOGIN_MEMBER = "loginMember";
 }
