5f53baee2f9918ecfcb0f84f3fa4942212bed9bb
hyeongcheolkim <tori1753@naver.com>
2022-10-01 05:14:57 +0900

    SwaggerConfig 삭제. 서버실행파일에 통합.

--- src/main/java/NaNSsoGong/MrDaeBakDining/MrDaeBakDiningApplication.java
+++ src/main/java/NaNSsoGong/MrDaeBakDining/MrDaeBakDiningApplication.java
@@ -1,6 +1,13 @@
 package NaNSsoGong.MrDaeBakDining;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,5 +24,29 @@ public class MrDaeBakDiningApplication {
 	@Bean
 	Hibernate5Module hibernate5Module(){
 		return new Hibernate5Module();
+	}
+
+	@Bean
+	public OpenAPI api() {
+		Info info = new Info()
+				.title("MrDaeBak Dinner Service")
+				.description("Software Engineering Project by Team.NaNSsoGong")
+				.version("V1.0")
+				.contact(new Contact()
+						.name("Front WEB")
+						.url("https://uos.ac.kr"))
+				.license(new License()
+						.name("Apache License Version 2.0")
+						.url("http://www.apache.org/license/LICENSE-2.0"));
+
+		SecurityScheme auth = new SecurityScheme()
+				.type(SecurityScheme.Type.APIKEY)
+				.in(SecurityScheme.In.COOKIE)
+				.name("JSESSIONID");
+
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("basicAuth", auth))
+				.addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+				.info(info);
 	}
 }
