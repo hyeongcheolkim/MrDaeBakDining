63187452995c1a16a7599ad38219a290faf2aa5b
HyeongCheolKim_NoteBook <tori1753@naver.com>
2022-11-10 18:13:46 +0900

    feat: 디너정보조회 by DinnerId 푸드가격정보추가
    디너정보조회 by DinnerId 푸드가격정보추가

--- src/main/java/NaNSsoGong/MrDaeBakDining/domain/dinner/controller/response/DinnerFoodInfoResponse.java
+++ src/main/java/NaNSsoGong/MrDaeBakDining/domain/dinner/controller/response/DinnerFoodInfoResponse.java
@@ -8,10 +8,12 @@ public class DinnerFoodInfoResponse {
     private Long foodId;
     private String foodName;
     private Integer foodQuantity;
+    private Integer foodPrice;
 
     public DinnerFoodInfoResponse(DinnerFood dinnerFood) {
         this.foodId = dinnerFood.getFood().getId();
         this.foodName = dinnerFood.getFood().getName();
         this.foodQuantity = dinnerFood.getFoodQuantity();
+        this.foodPrice = dinnerFood.getFood().getSellPrice();
     }
 }
