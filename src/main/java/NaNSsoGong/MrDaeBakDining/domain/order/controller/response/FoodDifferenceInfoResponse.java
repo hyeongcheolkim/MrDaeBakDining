9f8bd0d9e61841bf5084d6b40f01231c67b27df6
hyeongcheolkim <tori1753@naver.com>
2022-11-12 22:40:04 +0900

    fix: 의미없는 데이터가 response에 포함되던 현상 수정
    주문수정 응답 json에 의미없는 데이터가 response에 포함되던 현상 수정

--- src/main/java/NaNSsoGong/MrDaeBakDining/domain/order/controller/response/FoodDifferenceInfoResponse.java
+++ src/main/java/NaNSsoGong/MrDaeBakDining/domain/order/controller/response/FoodDifferenceInfoResponse.java
@@ -5,13 +5,11 @@ import lombok.Data;
 
 @Data
 public class FoodDifferenceInfoResponse {
-    private Long orderSheetItemId;
     Long foodId;
     String foodName;
     Integer foodQuantity;
 
     public FoodDifferenceInfoResponse(FoodDifference foodDifference) {
-        this.orderSheetItemId = foodDifference.getId();
         this.foodId = foodDifference.getFood().getId();
         this.foodName = foodDifference.getFood().getName();
         this.foodQuantity = foodDifference.getFoodQuantity();
