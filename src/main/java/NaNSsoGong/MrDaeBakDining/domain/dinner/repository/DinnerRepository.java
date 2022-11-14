f819321c10e8c2cfefb81d57f74d7f21bb282dc5
hyeongcheolkim <tori1753@naver.com>
2022-11-14 22:27:25 +0900

    feat: 음성인식 개발에 따른 디너 리포지토리 메서드 추가
    디너 전체필드가아닌, 이름과 아이디만 질의이하는 메서드 추가

--- src/main/java/NaNSsoGong/MrDaeBakDining/domain/dinner/repository/DinnerRepository.java
+++ src/main/java/NaNSsoGong/MrDaeBakDining/domain/dinner/repository/DinnerRepository.java
@@ -3,6 +3,7 @@ package NaNSsoGong.MrDaeBakDining.domain.dinner.repository;
 import NaNSsoGong.MrDaeBakDining.domain.dinner.domain.Dinner;
 import NaNSsoGong.MrDaeBakDining.domain.food.domain.Food;
 import NaNSsoGong.MrDaeBakDining.domain.ingredient.domain.Ingredient;
+import NaNSsoGong.MrDaeBakDining.domain.voice.controller.response.DinnerNameAndIdDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,4 +16,5 @@ import java.util.Optional;
 public interface DinnerRepository extends JpaRepository<Dinner, Long> {
     Page<Dinner> findAllByEnable(Boolean enable, Pageable pageable);
     List<Dinner> findAllByName(String name);
+    List<DinnerNameAndIdDto> findAllByEnable(Boolean enable);
 }
