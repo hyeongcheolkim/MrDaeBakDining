df32402e43f4a14fdd4e6a34d2f6972952b2bc34
hyeongcheolkim <tori1753@naver.com>
2022-11-16 17:57:28 +0900

    feat: calculateTotalFoodQuantity 메서드 작성

--- src/main/java/NaNSsoGong/MrDaeBakDining/domain/food/service/FoodService.java
+++ src/main/java/NaNSsoGong/MrDaeBakDining/domain/food/service/FoodService.java
@@ -4,12 +4,16 @@ import NaNSsoGong.MrDaeBakDining.domain.dinner.domain.Dinner;
 import NaNSsoGong.MrDaeBakDining.domain.food.domain.Food;
 import NaNSsoGong.MrDaeBakDining.domain.food.repository.FoodRepository;
 import NaNSsoGong.MrDaeBakDining.domain.ingredient.repository.IngredientRepository;
+import NaNSsoGong.MrDaeBakDining.domain.order.domain.Order;
+import NaNSsoGong.MrDaeBakDining.domain.order.domain.OrderSheet;
+import NaNSsoGong.MrDaeBakDining.domain.order.service.OrderSheetService;
 import NaNSsoGong.MrDaeBakDining.domain.recipe.domain.Recipe;
 import NaNSsoGong.MrDaeBakDining.domain.ingredient.domain.Ingredient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,24 +24,25 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class FoodService {
     private final FoodRepository foodRepository;
+    private final OrderSheetService orderSheetService;
 
-    public Boolean isMakeAble(Food food){
-        if(!food.getEnable())
+    public Boolean isMakeAble(Food food) {
+        if (!food.getEnable())
             return false;
         List<Recipe> recipeList = food.getRecipeList();
-        for(var recipe : recipeList){
+        for (var recipe : recipeList) {
             Ingredient ingredient = recipe.getIngredient();
             Integer ingredientQuantity = recipe.getIngredientQuantity();
-            if(!ingredient.getEnable() || ingredient.getStockQuantity() < ingredientQuantity)
+            if (!ingredient.getEnable() || ingredient.getStockQuantity() < ingredientQuantity)
                 return false;
         }
         return true;
     }
 
-    public Map<Ingredient, Integer> makeFood(Food food){
+    public Map<Ingredient, Integer> makeFood(Food food) {
         var ret = new ConcurrentHashMap<Ingredient, Integer>();
         List<Recipe> recipeList = food.getRecipeList();
-        for(var recipe : recipeList){
+        for (var recipe : recipeList) {
             Ingredient ingredient = recipe.getIngredient();
             Integer ingredientQuantity = recipe.getIngredientQuantity();
             ingredient.setStockQuantity(ingredient.getStockQuantity() - ingredientQuantity);
@@ -50,5 +55,14 @@ public class FoodService {
         return foodRepository.findAllByName(name).stream()
                 .map(Food::getEnable)
                 .anyMatch(e -> e == true);
+    }
+
+    public Map<Food, Integer> calculateTotalFoodQuantity(Order order) {
+        List<OrderSheet> orderSheetList = order.getOrderSheetList();
+        Map<Food, Integer> foodAndQuantity = new HashMap<>();
+        for (var orderSheet : orderSheetList)
+            orderSheetService.calculatedFoodAndQuantity(orderSheet)
+                    .forEach((key, value) -> foodAndQuantity.merge(key, value, (v1, v2) -> v1 + v2));
+        return foodAndQuantity;
     }
 }
