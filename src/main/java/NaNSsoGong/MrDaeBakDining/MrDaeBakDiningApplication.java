91833ebe1d2e684a79fb38199f20befa13af6575
hyeongcheolkim <tori1753@naver.com>
2022-11-18 02:00:18 +0900

    feat: swagger에 local서버와 prod서버 request구분

--- src/main/java/NaNSsoGong/MrDaeBakDining/MrDaeBakDiningApplication.java
+++ src/main/java/NaNSsoGong/MrDaeBakDining/MrDaeBakDiningApplication.java
@@ -8,11 +8,14 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
 public class MrDaeBakDiningApplication {
@@ -58,7 +61,16 @@ public class MrDaeBakDiningApplication {
 				.in(SecurityScheme.In.COOKIE)
 				.name("JSESSIONID");
 
+		Server local = new Server();
+		local.setDescription("local");
+		local.setUrl("http://localhost:8080");
+
+		Server prod = new Server();
+		prod.setDescription("prod");
+		prod.setUrl("https://mrdaebakservice.kro.kr");
+
 		return new OpenAPI()
+				.servers(List.of(local, prod))
 				.components(new Components().addSecuritySchemes("basicAuth", auth))
 				.addSecurityItem(new SecurityRequirement().addList("basicAuth"))
 				.info(info);
